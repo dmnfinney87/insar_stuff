3d0310948e274e21220e8c66be04c24c52629c5c
dhs <dhs@dhss-iMac.local>
2017-01-14 22:52:07 -0800

    Third commit

--- create_xml.py
+++ create_xml.py
@@ -1,9 +1,12 @@
+#given a directory with two ALOS folders, this will create an .xml script for ISCE
+#dhs january 13, 2017
+
 import csv
 import os
+
 print ('enter path')
 
-#looks for directory
 
-#
+
 
 
